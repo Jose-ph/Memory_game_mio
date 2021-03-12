16a29b13e7264120d6a042471a884fb7549817df
Jose <jose_esteban_basualdo@hotmail.com>
2021-03-12 17:01:12 -0300

    agrega colores random
    se prepara el tablero para que cada vez que se recargue la página los colores aparezcan en un lugar random

---
+++ src/main.js
@@ -0,0 +1,34 @@
+const tablero = document.querySelector('#tablero');
+const estadoJuego = document.querySelector('#estado');
+const numeroJugadas = document.querySelector("#jugadas");
+
+estadoJuego.textContent = "Preparando el juego...";
+
+let cartas  = document.querySelectorAll(".cartas");
+
+let colores = ["rojo","azul","amarillo","verde","negro","celeste"];
+
+let coloresDuplicados = colores.concat(colores);
+
+
+function prepararTablero (){
+
+
+    asignarColorRandom(coloresDuplicados);
+}
+
+function asignarColorRandom (colores) {
+
+  
+    coloresRandom = colores.sort( () => .5 - Math.random() );
+
+   for(let i = 0 ; i <= cartas.length ; i++){
+
+       cartas[i].classList.add(coloresRandom[i]);
+   }
+  
+
+}
+
+prepararTablero();
+
