a1f1c37c87cb3a17176bcdbde3973772cb864113
Jose <jose_esteban_basualdo@hotmail.com>
2021-03-16 23:07:13 -0300

    agrega mostrarCarta
    En este punto al hacer click se muestra u oculta la carta  según esté de frente o de espaldas.Es necesario que primero se obtenga la carta que elige el usuario y luego se pase a la función.

--- src/main.js
+++ src/main.js
@@ -7,7 +7,13 @@ let colores = ["rojo","amarillo","verde","azul","violeta","gris"];
 
 let coloresDuplicados = colores.concat(colores);
 
+
+ocultarFrenteCartas(cartas);
 asignarColorRandomCartas(cartas,coloresDuplicados);
+obtenerCartaUsuario();
+//mostrarCarta();
+
+
 
 
 function asignarColorRandomCartas(cartas,colores){
@@ -20,5 +26,57 @@ function asignarColorRandomCartas(cartas,colores){
 
     }
 
+}
 
+function ocultarFrenteCartas (cartas){
+
+    for( let i = 0 ; i < cartas.length ; i++){
+
+        cartas[i].classList.add("dorso");
+    }
+
+
+}
+
+function mostrarCarta (){
+
+   
+    for( let i = 0 ; i < cartas.length ; i++){
+
+        cartas[i].addEventListener('click', (e)=>{
+
+            let cartaElegida = e.target;
+
+            let cartaDeEspaldas = cartaElegida.classList.contains("dorso");
+            let cartaDeFrente = !cartaElegida.classList.contains("dorso");
+
+            if (cartaDeEspaldas){
+
+                cartaElegida.classList.remove("dorso");
+            }
+
+            else if (cartaDeFrente){
+
+                cartaElegida.classList.add("dorso");
+            }
+            
+    
+        });
+
+    }
+
+}
+
+function obtenerCartaUsuario (){
+
+    tablero.onclick = function(e) {
+
+        const $cartaElegida = e.target;
+       
+        
+
+
+    }
+    
+    return console.log($cartaElegida);
 }
